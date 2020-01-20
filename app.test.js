f37a24db2062eeaec003ce8c03164230e2fe90fc
Ainsley Crawford <ainsley.c@hotmail.co.uk>
2020-01-20 16:03:33 +0000

    Six test cases pass

--- app.test.js
+++ app.test.js
@@ -1,10 +1,32 @@
 const returnCapital = require('./app')
 
-test('Returns the capital of a given country or region.', () => {
+test('Returns the capital of a given European country or region.', () => {
+    expect(returnCapital("Bosnia and Herzegovina")).toBe("Sarajevo")
+    expect(returnCapital("Cyprus")).toBe("Nicosia")
+    expect(returnCapital("Lithuania")).toBe("Vilnius")
+})
+test('Returns the capital of a given North American country or region.', () => {
+    expect(returnCapital("Cuba")).toBe("Havana")
     expect(returnCapital("Jamaica")).toBe("Kingston")
+    expect(returnCapital("Quebec")).toBe("Quebec City")
+})
+test('Returns the capital of a given South American country or region.', () => {
     expect(returnCapital("Guyana")).toBe("Georgetown")
-    expect(returnCapital("Trinidad and Tobago")).toBe("Port of Spain")
-    expect(returnCapital("Guyana")).toBe("Georgetown")
-    expect(returnCapital("Jamaica")).toBe("Kingston")
-    expect(returnCapital("Guyana")).toBe("Georgetown")
+    expect(returnCapital("Quebec")).toBe("Quebec City")
+    expect(returnCapital("Suriname")).toBe("Paramaribo")
+})
+test('Returns the capital of a given African country or region.', () => {
+    expect(returnCapital("Liberia")).toBe("Monrovia")
+    expect(returnCapital("Mozambique")).toBe("Maputo")
+    expect(returnCapital("Senegal")).toBe("Dakar")
+})
+test('Returns the capital of a given Asian country or region.', () => {
+    expect(returnCapital("Kyrgyzstan")).toBe("Bishkek")
+    expect(returnCapital("South Korea")).toBe("Seoul")
+    expect(returnCapital("Taiwan")).toBe("Taipei")
+})
+test('Returns the capital of a given Oceanian country or region.', () => {
+    expect(returnCapital("Australia")).toBe("Canberra")
+    expect(returnCapital("New Zealand")).toBe("Wellington")
+    expect(returnCapital("Vanuatu")).toBe("Port Vila")
 })
